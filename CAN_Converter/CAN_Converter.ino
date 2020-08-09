af22dc2e19c2e541f9c81675d3cb5de17ce35641
cypher1024 <cypher1024@users.noreply.github.com>
2020-08-09 19:17:41 +1000

    Use separate modules for receiving and transmitting

--- CAN_Converter/CAN_Converter.ino
+++ CAN_Converter/CAN_Converter.ino
@@ -1,5 +1,5 @@
 #include <SPI.h>
-#include <mcp2515.h>
+#include <mcp2515.h>  // https://github.com/autowp/arduino-mcp2515
 
 // Master debugging switch
 //#define SERIAL_DEBUG_ENABLED
@@ -25,16 +25,24 @@ const uint32_t ID_LIST[] = {
 const uint8_t TIMING_PULSE_OUTPUT = 2;
 #define       TIMING_PULSE_REGISTER PORTD
 #define       TIMING_PULSE_PINMASK  PD2
-const uint8_t MCP2515_CHIP_SELECT = 10;
+const uint8_t TRANSMITTER_CHIP_SELECT = 9;
+const uint8_t RECEIVER_CHIP_SELECT = 10;
+const uint8_t UNO_SPI_MOSI = 11;
+const uint8_t UNO_SPI_MISO = 12;
+const uint8_t UNO_SPI_SCK  = 13;
+const uint8_t MEGA_SPI_MISO = 50;
+const uint8_t MEGA_SPI_MOSI = 51;
+const uint8_t MEGA_SPI_SCK  = 52;
 
-MCP2515 mcp2515(MCP2515_CHIP_SELECT);
+MCP2515 receiver(RECEIVER_CHIP_SELECT);
+MCP2515 transmitter(TRANSMITTER_CHIP_SELECT);
 
 #ifdef SERIAL_DEBUG_ENABLED
     bool show_message = true;
 #endif
 
 
-void set_id_filters(){
+void set_id_filters() {
     // This builds a filter and mask based on the common bits from every ID value
     // e.g. 10100111001  ID 0
     //      00110110100  ID 1
@@ -42,19 +50,19 @@ void set_id_filters(){
     //      01000110010  Mask
     //      00000110000  Filter
     // Note that filters and their masks are always 29 bits, even when applied to
-    // standard 11 bit ID frames. 16 of those bits will match the first 16 bits of the 
+    // standard 11 bit ID frames. 16 of those bits will match the first 16 bits of the
     // data if their associated mask bits are set. It's best to avoid that
     // For an 11 bit ID:
     // <--  MSB                  LSB -->
-    //                       [   id    ] 
+    //                       [   id    ]
     // 010101010101010101111 00011110000
     
-    mcp2515.setConfigMode();  // Must be in config mode to set filters
+    receiver.setConfigMode();  // Must be in config mode to set filters
     uint32_t id_filter = 0;
     uint32_t id_mask = 0;
-    for (uint8_t id_num=0; id_num<sizeof(ID_LIST)/4; id_num++){
+    for (uint8_t id_num = 0; id_num < sizeof(ID_LIST) / 4; id_num++) {
         #ifdef SERIAL_DEBUG_ENABLED
-            Serial.print(F("Adding ID 0x")); Serial.print(ID_LIST[id_num], HEX); 
+            Serial.print(F("Adding ID 0x")); Serial.print(ID_LIST[id_num], HEX);
             Serial.println(F(" to the filter..."));
         #endif
         
@@ -62,11 +70,11 @@ void set_id_filters(){
             id_filter = ID_LIST[id_num];
             id_mask = 0b11111111111;
         }
-        else{
-            for (uint8_t bit_num=0; bit_num<11; bit_num++){
-                if (bitRead(id_mask, bit_num)){
+        else {
+            for (uint8_t bit_num = 0; bit_num < 11; bit_num++) {
+                if (bitRead(id_mask, bit_num)) {
                     // Mask bit for this position is still enabled
-                    if (bitRead(id_filter, bit_num) != bitRead(ID_LIST[id_num], bit_num)){
+                    if (bitRead(id_filter, bit_num) != bitRead(ID_LIST[id_num], bit_num)) {
                         // Bit at position bit_num differs from that bit in the filter
                         // Disable that mask bit
                         bitClear(id_mask, bit_num);
@@ -75,80 +83,86 @@ void set_id_filters(){
             }
         }
     }
-
+    
     // Set the filters & masks for both RX buffers to the same thing
     // This can be changed if we need more throughput (or finer filtering) later
-    mcp2515.setFilterMask(MCP2515::MASK0, false, id_mask); // RXB0
-    mcp2515.setFilter(MCP2515::RXF0, false, id_filter);    // RXB0
-    mcp2515.setFilterMask(MCP2515::MASK1, false, id_mask); // RXB1
-    mcp2515.setFilter(MCP2515::RXF2, false, id_filter);    // RXB1
+    receiver.setFilterMask(MCP2515::MASK0, false, id_mask); // RXB0
+    receiver.setFilter(MCP2515::RXF0, false, id_filter);    // RXB0
+    receiver.setFilterMask(MCP2515::MASK1, false, id_mask); // RXB1
+    receiver.setFilter(MCP2515::RXF2, false, id_filter);    // RXB1
 }
 
 
-void setup(){
-    //  Set up the serial port (if required) 
+void setup() {
+    //  Set up the serial port (if required)
     #ifdef SERIAL_DEBUG_ENABLED
-        while (!Serial);
-        Serial.begin(115200);
+      while (!Serial);
+      Serial.begin(115200);
     #endif
 
     // Set up the timing pin
     #ifdef TIMING_PULSES_ENABLED
-        pinMode(TIMING_PULSE_OUTPUT, OUTPUT);
-        digitalWrite(TIMING_PULSE_OUTPUT, HIGH);
+      pinMode(TIMING_PULSE_OUTPUT, OUTPUT);
+      digitalWrite(TIMING_PULSE_OUTPUT, HIGH);
     #endif
 
-    // Set up MCP2515
-    mcp2515.reset();
-    MCP2515::ERROR set_bitrate_result = mcp2515.setBitrate(SPEED, CLOCK);
+    // Set up MCP2515 modules
+    receiver.reset();
+    MCP2515::ERROR set_receiver_bitrate_result = receiver.setBitrate(SPEED, CLOCK);
     set_id_filters();
-    mcp2515.setNormalMode();
-    
+    receiver.setNormalMode();
+    transmitter.reset();
+    MCP2515::ERROR set_transmitter_bitrate_result = receiver.setBitrate(SPEED, CLOCK);
+    transmitter.setNormalMode();
+
     #ifdef SERIAL_DEBUG_ENABLED
-        if (set_bitrate_result == MCP2515::ERROR_OK){
+        if (set_receiver_bitrate_result == MCP2515::ERROR_OK && set_transmitter_bitrate_result == MCP2515::ERROR_OK) {
             Serial.println(F("Sucessfully set bitrate"));
         }
-        else{
-            Serial.println(F("Failed to set bitrate. Is the MCP2515 connected?"));
+        else if (set_receiver_bitrate_result != MCP2515::ERROR_OK){
+            Serial.println(F("Failed to set receiver bitrate. Is the MCP2515 connected?"));
+        }
+        else (set_receiver_bitrate_result != MCP2515::ERROR_OK){
+            Serial.println(F("Failed to set transmitter bitrate. Is the MCP2515 connected?"));
         }
         Serial.println(F("Finished setup"));
     #endif
 }
 
 
-void loop(){
+void loop() {
     #ifdef SERIAL_DEBUG_ENABLED
         #ifdef DEBUG_RATE_LIMIT
-            static uint32_t last_debug_message_time = 0;
-            if (millis() - last_debug_message_time >= 1000){
-                show_message = true;
-                last_debug_message_time = millis();
-            }
-            else{
-                show_message = false;
-            }
-        #endif 
+          static uint32_t last_debug_message_time = 0;
+          if (millis() - last_debug_message_time >= 1000) {
+            show_message = true;
+            last_debug_message_time = millis();
+          }
+          else {
+            show_message = false;
+          }
+        #endif
     #endif
-    
+
     // Check for a new frame
     static struct can_frame incoming_frame;
-    if (mcp2515.readMessage(&incoming_frame) == MCP2515::ERROR_OK){
+    if (receiver.readMessage(&incoming_frame) == MCP2515::ERROR_OK) {
         #ifdef SERIAL_DEBUG_ENABLED
-            if (show_message){
-                Serial.println(); Serial.print(F("Received: "));
-                Serial.print(F("ID: ")); Serial.print(incoming_frame.can_id, HEX);
-                Serial.print(F(", DLC:")); Serial.print(incoming_frame.can_dlc, HEX);
-                Serial.print(F(", Data: "));
-                for (int i = 0; i < incoming_frame.can_dlc; i++)  {
-                    Serial.print(incoming_frame.data[i], HEX);
-                    Serial.print(F(" "));
-                }
-                Serial.println();
+            if (show_message) {
+              Serial.println(); Serial.print(F("Received: "));
+              Serial.print(F("ID: ")); Serial.print(incoming_frame.can_id, HEX);
+              Serial.print(F(", DLC:")); Serial.print(incoming_frame.can_dlc, HEX);
+              Serial.print(F(", Data: "));
+              for (int i = 0; i < incoming_frame.can_dlc; i++)  {
+                  Serial.print(incoming_frame.data[i], HEX);
+                  Serial.print(F(" "));
+              }
+              Serial.println();
             }
         #endif
-
+        
         // Process the new frame
-        handle_haltech_frame(&incoming_frame, &mcp2515);
+        handle_haltech_frame(&incoming_frame, &transmitter);
     }
 
     // Output timing pulse
--- README.md
+++ README.md
@@ -9,12 +9,20 @@
 - Clone this repo or download it as a .zip and extract it
 - Download and install [this Arduino MCP2515 library](https://github.com/autowp/arduino-mcp2515#library-installation) 
 - Compile the firmware and flash it to your arduino of choice
-- Swap a 20 MHz crystal and 15 pF capacitors onto the MCP2515 module (the standard 8 MHz crystal isn't fast enough for 1 Mbps). If you want to use the standard 8 MHz crystal for testing, you'll need to set the CLOCK constant to MCP_8MHZ
-- Connect the MCP2515 module. If you're using an Uno, use the following pins:
-  - Arduino GND -> MCP2515 module GND
-  - Arduino D10 -> MCP2515 module CS
-  - Arduino D11 -> MCP2515 module SI
-  - Arduino D12 -> MCP2515 module SO
-  - Arduino D11 -> MCP2515 module SCK
+- Swap a 20 MHz crystal and 15 pF capacitors onto the MCP2515 modules (the standard 8 MHz crystal isn't fast enough for 1 Mbps). If you want to use the standard 8 MHz crystal for testing, you'll need to set the CLOCK constant to MCP_8MHZ
+- Connect the MCP2515 modules. If you're using an Uno, use the following pins:
+  - Uno GND -> Both MCP2515 GND
+  - Uno  D9 -> Transmitter MCP2515 CS
+  - Uno D10 -> Receiver MCP2515 CS
+  - Uno D11 -> Both MCP2515 module SI
+  - Uno D12 -> Both MCP2515 module SO
+  - Uno D11 -> Both MCP2515 module SCK
+- For a Mega 2560:
+  - Mega 2560 GND -> Both MCP2515 GND
+  - Mega 2560  D9 -> Transmitter MCP2515 CS
+  - Mega 2560 D10 -> Receiver MCP2515 CS
+  - Mega 2560 D50 -> Both MCP2515 module SO
+  - Mega 2560 D51 -> Both MCP2515 module SI
+  - Mega 2560 D52 -> Both MCP2515 module SCK
 
 Datasheet for the MCP2515: http://ww1.microchip.com/downloads/en/DeviceDoc/MCP2515-Stand-Alone-CAN-Controller-with-SPI-20001801J.pdf
